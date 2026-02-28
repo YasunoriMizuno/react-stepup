7e9af457e82fb236633144f6be14e647bc3a9082
YasunoriMizuno <yasunorimizuno@icloud.com>
2026-03-01 01:30:32 +0900

    feat: useCallbackとmemoを組み合わせた最適化の完成

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,19 @@ import { ChildArea } from "./ChildArea";
 
 export default function App() {
 console.log("App");
-const [count, setCount] = useState(0) ;
-const
-onClickCountUp = () => {
-setCount (count + 1) ;
-};
+const [text, setText] = useState('') ;
+const [open, setOpen] = useState(false) ;
+const onChangeText = (e) => setText(e.target.value);
+const onClickOpen = () => setOpen(!open) ;
+const onClickClose = () => setOpen(false) ;
+
 return (
   <div className="App">
-    <input />
+    <input value={text} onChange={onChangeText} />
     <br />
     <br />
-    <button>表示</button>
-    <ChildArea />
+    <button onClick={onClickOpen}>表示</button>
+    <ChildArea open={open} />
 </div>
 );
 }
